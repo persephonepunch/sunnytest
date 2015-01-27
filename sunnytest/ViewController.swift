74cc511f2bc42f1aa8e9b93faad72304e5b8e2b3
yoonsun lee <yoonsunlee150@gmail.com>
2015-01-26 22:44:50 -0500

    Initial Commit

---
+++ sunnytest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sunnytest
+//
+//  Created by YOONSUN LEE on 1/26/15.
+//  Copyright (c) 2015 YOONSUN LEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
